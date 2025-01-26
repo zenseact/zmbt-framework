33162c0a5af53960484d3f246e2117557fd369e9
Horolsky <oleksandr.khorolskyi@zenseact.com>
2025-01-26 13:15:40 +0200

    Add backends test pipeline

--- ci/linux-test-backend-cxx.sh
+++ ci/linux-test-backend-cxx.sh
@@ -6,14 +6,17 @@ readonly BAZEL_7_CONTAINER="gcr.io/bazel-public/bazel:7.4.1"
 readonly BAZEL_LATEST_CONTAINER="gcr.io/bazel-public/bazel:latest"
 ROOT_DIR="$(realpath $(dirname ${0})/..)"
 
-# Test the Bazel 7.4.1 build with GCC
+echo "Test the Bazel 7.4.1 build with GCC"
+
 time docker run \
   -e USER="$(id -u)" \
   -u="$(id -u)" \
   -v="${ROOT_DIR}:/src" \
+  -v="/tmp/build_output:/tmp/build_output" \
   -w="/src" \
   --rm \
   --env="CC=/usr/bin/gcc" \
   --env=CXXFLAGS="-Werror -Wdeprecated" \
   ${BAZEL_7_CONTAINER} \
+  --output_user_root=/tmp/build_output \
   test //...
