325624bb572524adf351ebec20c022a5fe202edb
Horolsky <oleksandr.khorolskyi@zenseact.com>
2025-02-07 16:54:16 +0200

    Move codegen to Python package

---
+++ codegen/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.0"
+
+
+import os
+
+
+WORKDIR = os.path.dirname(os.path.dirname(__file__))
--- zmbt-framework/backends/cxx/codegen.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-bazel run //templates/expr:keyword_hpp > zmbt-framework/backends/cxx/include/zmbt/model/keyword.hpp
-bazel run //templates/expr:keyword_cpp > zmbt-framework/backends/cxx/src/model/keyword.cpp
-bazel run //templates/expr:keyword_grammar_cpp > zmbt-framework/backends/cxx/src/model/keyword_grammar.cpp
-bazel run //templates/expr:expression_api_hpp > zmbt-framework/backends/cxx/include/zmbt/model/expression_api.hpp
-bazel run //templates/expr:expression_api_cpp > zmbt-framework/backends/cxx/src/model/expression_api.cpp
