58083e161c3f61ee0b9ba78994e23334f1e44156
Horolsky <oleksandr.khorolskyi@zenseact.com>
2025-12-02 11:18:50 +0200

    Fix ODR in zmbt_main_boost

--- zmbt-framework/backends/cxx/src/main_boost.cpp
+++ zmbt-framework/backends/cxx/src/main_boost.cpp
@@ -10,7 +10,6 @@
 
 #include <zenseact-mbt.hpp>
 
-// Called by boost::unit_test::unit_test_main before running tests.
 boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[])
 {
     zmbt::InitZmbt(argc, argv);
@@ -19,11 +18,5 @@ boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[])
         zmbt::format_failure_report(ss, sts);
         BOOST_ERROR(ss.str());
     });
-    return nullptr;  // use the default master test suite
-}
-
-// entry point:
-int main(int argc, char* argv[])
-{
-    return boost::unit_test::unit_test_main(&init_unit_test_suite, argc, argv);
+    return nullptr;
 }
